5967ca51bc766728f7afaa55a4e59dc3c2baa814
Agnieszka Klekot <74aklekot@gmail.com>
2024-10-07 20:48:48 +0200

    class Cart

--- src/js/script.js
+++ src/js/script.js
@@ -31,12 +31,18 @@
         linkIncrease: 'a[href="#more"]',
       },
     },
+    cart: {
+      toggleTrigger: '.cart__summary', 
+    },
   };
 
   const classNames = {
     menuProduct: {
       wrapperActive: 'active',
       imageVisible: 'active',
+    },
+    cart: {
+      wrapperActive: 'active', 
     },
   };
 
@@ -247,6 +253,34 @@
     }
   }
 
+
+  /* class Cart */
+  class Cart {
+    constructor(element) {
+      const thisCart = this;
+
+      thisCart.products = [];
+      thisCart.getElements(element);
+      console.log('new Cart ', thisCart);
+      thisCart.initActions();
+    }
+
+    getElements(element) {
+      const thisCart = this;
+
+      thisCart.dom = {};
+      thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+
+    initActions() {
+      const thisCart = this;
+      thisCart.dom.toggleTrigger.addEventListener('click', function () {
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
+    }
+  }
+
   const app = {
     initMenu: function () {
       const thisApp = this;
@@ -261,11 +295,25 @@
       thisApp.data = dataSource;
     },
 
+    initCart: function () {
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      if (cartElem) {
+        thisApp.cart = new Cart(cartElem);
+      } else {
+        console.error('Cart element not found');
+      }
+    },
+
     init: function () {
       const thisApp = this;
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
+
+
 
   };
 
